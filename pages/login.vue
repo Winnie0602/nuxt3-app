981d8abf4f38b61007aaf10b891c57ea2a62ce74
Winnie Chang <z976200@gmail.com>
2023-08-14 18:13:59 +0800

    新增表單驗證功能 vee-validate yup

--- pages/login.vue
+++ pages/login.vue
@@ -11,39 +11,49 @@
           </p>
         </div>
         <div class="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
-          <div class="card-body">
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Email</span>
-                <pre>values: {{ values }}</pre>
-              </label>
-              <input
-                v-bind="email"
-                type="text"
-                placeholder="email"
-                class="input input-bordered"
-              />
+          <form action="" @submit="onSubmit">
+            <div class="card-body">
+              <div class="form-control">
+                <div class="form-control w-full max-w-xs">
+                  <label class="label">
+                    <span class="label-text">Email</span>
+                    <span class="label-text-alt">{{ errors.email }}</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    class="input input-bordered w-full max-w-xs"
+                    v-bind="email"
+                  />
+                  <label class="label">
+                    <span class="label-text-alt"></span>
+                    <span class="label-text-alt"></span>
+                  </label>
+                </div>
+              </div>
+              <div class="form-control">
+                <div class="form-control w-full max-w-xs">
+                  <label class="label">
+                    <span class="label-text">Password</span>
+                    <span class="label-text-alt">{{ errors.password }}</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    class="input input-bordered w-full max-w-xs"
+                    v-bind="password"
+                  />
+                  <label class="label">
+                    <span class="label-text-alt"></span>
+                    <span class="label-text-alt"></span>
+                  </label>
+                </div>
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-primary">Login</button>
+              </div>
             </div>
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Password</span>
-              </label>
-              <input
-                v-bind="password"
-                type="text"
-                placeholder="password"
-                class="input input-bordered"
-              />
-              <label class="label">
-                <a href="#" class="link-hover link label-text-alt"
-                  >Forgot password?</a
-                >
-              </label>
-            </div>
-            <div class="form-control mt-6">
-              <button class="btn btn-primary">Login</button>
-            </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
@@ -51,11 +61,37 @@
 </template>
 
 <script setup lang="ts">
-import { useForm } from 'vee-validate'
-const { values, defineInputBinds } = useForm()
+import { useForm, configure } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/yup'
+import * as yup from 'yup'
+import { setLocale } from 'yup'
+import zhHant from '~/utils/zh-hant.json'
+
+configure({
+  bails: false,
+  validateOnModelUpdate: false,
+  validateOnChange: false,
+  validateOnBlur: false,
+})
+
+setLocale(zhHant)
+
+const { errors, defineInputBinds, handleSubmit } = useForm({
+  validationSchema: toTypedSchema(
+    yup.object({
+      email: yup.string().email().required(),
+      password: yup.string().min(6).required(),
+    }),
+  ),
+})
+
+const onSubmit = handleSubmit((values) => {
+  alert(JSON.stringify(values, null, 2))
+})
 
 const email = defineInputBinds('email')
 const password = defineInputBinds('password')
+
 definePageMeta({
   layout: false,
 })
