1a9e8929e8e819e94eb4dcd58e7b785a5cafbfd8
winniechang <winniechang@mirrormedia.mg>
2023-12-12 18:10:55 +0800

    upd Embla

--- components/Embla.vue
+++ components/Embla.vue
@@ -1,25 +1,75 @@
 <script setup lang="ts">
 import emblaCarouselVue from 'embla-carousel-vue'
 
-const [emblaNode] = emblaCarouselVue({ dragFree: true, align: 'end' })
+const [emblaNode, emblaApi] = emblaCarouselVue({
+  dragFree: true,
+  align: 'end',
+  // active: false,
+})
+
+const next = ref()
+const prev = ref()
+
+// 上一頁
+const scrollPrev = () => {
+  emblaApi.value?.scrollPrev()
+
+  if (!emblaApi.value?.canScrollPrev()) {
+    prev.value.style.display = 'none'
+  } else {
+    next.value.style.display = 'block'
+  }
+}
+
+// 下一頁
+const scrollNext = () => {
+  emblaApi.value?.scrollNext()
+  console.log(emblaApi.value?.scrollProgress())
+  if (!emblaApi.value?.canScrollNext()) {
+    next.value.style.display = 'none'
+  } else {
+    prev.value.style.display = 'block'
+  }
+}
 </script>
 
 <template>
-  <div class="embla" ref="emblaNode">
-    <div class="embla__container">
-      <div
-        v-for="item in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
-        :key="item"
-        class="embla__slide h-[300px] w-[500px] flex-[0_0_100%] border border-white bg-purple-300 sm:flex-[0_0_50%] lg:flex-[0_0_25%]"
-      >
-        {{ item }}
+  <div
+    class="embla relative flex w-full items-center md:w-[700px] xl:w-[1280px]"
+  >
+    <button
+      ref="prev"
+      class="embla__prev btn btn-info btn-outline absolute -left-[30px] z-50 hidden"
+      @click="scrollPrev"
+    >
+      Prev
+    </button>
+    <div
+      class="embla__viewport w-full max-w-[95vw] md:w-[700px] xl:w-[1280px]"
+      ref="emblaNode"
+    >
+      <div class="embla__container">
+        <div
+          v-for="item in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]"
+          :key="item"
+          class="embla__slide mr-6 h-[180px] flex-[0_0_150px] bg-purple-300 lg:mr-8 xl:flex-[0_0_155px]"
+        >
+          {{ item }}
+        </div>
       </div>
     </div>
+    <button
+      ref="next"
+      class="embla__next btn btn-info btn-outline absolute -right-[30px] hidden sm:block"
+      @click="scrollNext"
+    >
+      Next
+    </button>
   </div>
 </template>
 
 <style scoped>
-.embla {
+.embla__viewport {
   overflow: hidden;
 }
 .embla__container {
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <Navbar />
-  <div class="h-auto md:container md:mx-auto md:min-h-[calc(100vh-68px-220px)]">
+  <div
+    class="mx-0 flex h-auto justify-center md:container md:mx-auto md:min-h-[calc(100vh-68px-220px)]"
+  >
     <slot />
   </div>
 
